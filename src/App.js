0550cb5ea3623a1998626aa8811ddb6e566e7af1
Ali Djellalil <alidjellalil@gmail.com>
2022-10-07 17:26:52 +0200

    third commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    <div TOP DIV REACT CONFIG> 
+      <div FULLPAGE SECTION className="bg-white w-scree h-screen">
+        <div TOP NAVBAR className="
+          font-Montserat font-bold text-center text-2xl text-blue-500
+          w-screen h-11
+          border border-sm border to-black border shadow-md">
+          The React Digital Playground
+        </div>
+        <div DIV SITES className="w-screen h-screen p-2 m-2
+        flex">
+          <div className="w-[10%] h-[20%]
+          border border-blue-500 border rounded-md border shadow-md
+          font-Montserat font-bold text-center text-2xl text-blue-500
+          py-9 px-9 my-9 mx-9
+          flex flex-initial"
+          ><a href="">THE SITE</a></div>
+
+
+        </div>
+
+
+
+      </div>
     </div>
   );
 }
---
+++ src/components/TheAlertButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const TheAlertButton = () => {
+  return (
+    <div>
+      
+
+
+
+
+      
+    </div>
+  )
+}
+
+export default TheAlertButton
